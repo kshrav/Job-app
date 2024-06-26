52f68ebf30f83f0ed59779fe1b4f201722cba0ad
kshrav <shravyakatukojwala814@gmail.com>
2024-06-27 00:30:43 +0530

    Adding new file

---
+++ src/test/java/shravya/spring/learning/spring_demo/SpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package shravya.spring.learning.spring_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
